a64fd212ef21363b1bfe7fd21696f4a5158cb915
adisethupathy <adisethupathy@gmail.com>
2018-05-24 14:50:30 -0400

    6

--- src/problem6.py
+++ src/problem6.py
@@ -2,8 +2,8 @@
 Final exam, problem 3.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Adi Sethupathy.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -75,7 +75,7 @@ And this one for n=14:
     :type n: int
     """
     # ------------------------------------------------------------------
-    # TODO: Implement and test this function.
+    # done: Implement and test this function.
     #          Some tests are already written for you (above).
     ####################################################################
     # IMPORTANT: In solving this problem,
@@ -87,6 +87,27 @@ And this one for n=14:
     #   print Xs for the spaces until you figure out where the problem is
     #   (and then change the Xs back to spaces).
     # ------------------------------------------------------------------
+
+    for k in range(n):
+        for a in range(n-k + 1):
+            spaces = n - (k + 1)
+            print(' ', end='' * spaces)
+        for b in range(k+1):
+            z = b
+            if z >= 9:
+                z = b - 10
+            print(z+1, end='')
+        print(' ', end='')
+        for c in range(k + 2):
+            print('*', end='')
+        print(' ', end='')
+        for d in range(n - k):
+            x = (n-k)-d
+            if x >= 10:
+                x -= 10
+            print(x, end='')
+        print()
+
 
 
 # ----------------------------------------------------------------------
