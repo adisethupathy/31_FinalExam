266cc5e958e164c1ad7a3c0eefca35af74f00878
adisethupathy <adisethupathy@gmail.com>
2018-05-24 14:00:38 -0400

    5

--- src/problem5.py
+++ src/problem5.py
@@ -2,9 +2,9 @@
 Final exam, problem 5.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and Adi Sethupathy.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -136,9 +136,17 @@ def problem5(m, numbers1, numbers2):
       :type [str]
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # done: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+
+    new = []
+
+    for k in range(len(numbers1)):
+        if numbers1[k] * m == numbers2[k]:
+            new += [numbers1[k]]
+            new += [numbers2[k]]
+    return new
 
 
 ###############################################################################
